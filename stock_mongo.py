0d067192fbad8b41d193064588c1892892d0c52e
junwu0122 <jun860122@gmail.com>
2025-09-03 19:32:26 +0800

    Update stock_mongo.py

--- stock_mongo.py
+++ stock_mongo.py
@@ -6,17 +6,21 @@ import datetime
 load_dotenv()
 
 MONGO_URL = os.getenv("MONGODB_URL")
-DATABASE_NAME = 'Jun'
+database_name = 'Jun'
 COLLECTION_NAME = 'stock'
 
 def init_mongo_db():
     client = MongoClient(MONGO_URL)
-    db = client[DATABASE_NAME]
+    db = client[database_name]
     return db
 
 def get_stock():
     db = init_mongo_db()
-    return list(db[COLLECTION_NAME].find({}))
+    cursor = db[COLLECTION_NAME].find(
+        {}, 
+        {"_id":0, "stock_name":1, "price":1, "operator":1, "notified":1, "datetime":1}
+    )
+    return list(cursor)
 
 def get_stock_by_name(name):
     db = init_mongo_db()
@@ -24,9 +28,11 @@ def get_stock_by_name(name):
 
 def add_stock(name, price, operator="less_than"):
     if operator not in ["less_than", "greater_than"]:
-        raise ValueError("operator 只能是 less_than 或 greater_than")
+        raise ValueError("operator 只能是 'less_than' 或 'greater_than'")
+
     db = init_mongo_db()
     stocks = db[COLLECTION_NAME]
+
     existing = stocks.find_one({"stock_name": name})
 
     if existing is None:
@@ -38,6 +44,7 @@ def add_stock(name, price, operator="less_than"):
             "datetime": datetime.datetime.now(datetime.timezone.utc)
         }
         stocks.insert_one(price_dic)
+        print(f"✅ 已新增股票監控：{name}, {operator} {price}")
     else:
         new_values = {
             "$set": {
@@ -48,6 +55,7 @@ def add_stock(name, price, operator="less_than"):
             }
         }
         stocks.update_one({"stock_name": name}, new_values)
+        print(f"🔄 已更新股票監控：{name}, {operator} {price}")
 
 def update_notified_status(name, status: bool):
     db = init_mongo_db()
