3b7e0e507a917bd7290f4c8e1c03954b5b766b52
junwu0122 <jun860122@gmail.com>
2025-09-03 19:30:21 +0800

    Update line_message.py

--- line_message.py
+++ line_message.py
@@ -1,21 +1,13 @@
-from dotenv import load_dotenv
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
 import os
-from linebot.v3.messaging import Configuration, ApiClient, MessagingApi
-from linebot.v3.messaging.models import TextMessage, PushMessageRequest
+from dotenv import load_dotenv
 
-# 載入環境變數
 load_dotenv()
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 
-access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
-user_id = os.getenv("LINE_USER_ID")
-
-# 設定 API 金鑰
-configuration = Configuration(access_token=access_token)
-
-# 建立 API 實例並送出訊息
-def send_message(response):
-    with ApiClient(configuration) as api_client:
-        messaging_api = MessagingApi(api_client)
-        message = TextMessage(text=response)
-        push_request = PushMessageRequest(to=user_id, messages=[message])
-        messaging_api.push_message(push_message_request=push_request)
+def send_message(text, user_id=None):
+    if user_id:
+        line_bot_api.push_message(user_id, TextSendMessage(text=text))
+    else:
+        print(f"[DEBUG] {text}")
